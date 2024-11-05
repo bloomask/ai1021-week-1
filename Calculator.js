15c3d7b53ae710e6848233b4a74884377f659aea
fdlt3669 <elvin.alirzayev@lt.hs-fulda.de>
2024-11-05 17:45:56 +0100

    implement add and subtract methods in Calculator class

--- Calculator.js
+++ Calculator.js
@@ -1,55 +1,49 @@
 class Calculator {
-    /**
-     * @param {number} value
-     */
-    constructor(value) {
-        this.value = value;
-    }
+  /**
+   * @param {number} value
+   */
+  constructor(value) {
+    this.value = value;
+  }
 
-    /**
-     * @param {number} value
-     * @return {Calculator}
-     */
-    add(value){
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  add(value) {
+    this.result += value;
+    return this;
+  }
 
-    }
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
 
-    /**
-     * @param {number} value
-     * @return {Calculator}
-     */
-    subtract(value){
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  multiply(value) {}
 
-    }
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  divide(value) {}
 
-    /**
-     * @param {number} value
-     * @return {Calculator}
-     */
-    multiply(value) {
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  power(value) {}
 
-    }
-
-    /**
-     * @param {number} value
-     * @return {Calculator}
-     */
-    divide(value) {
-
-    }
-
-    /**
-     * @param {number} value
-     * @return {Calculator}
-     */
-    power(value) {
-
-    }
-
-    /**
-     * @return {number}
-     */
-    getResult() {
-
-    }
+  /**
+   * @return {number}
+   */
+  getResult() {}
 }
